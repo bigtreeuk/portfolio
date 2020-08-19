a0cf4e99993fa7121185850fd57f6fdb9fab93dc
bigtreeuk <michael@bigtreeuk.co.uk>
2020-08-19 10:17:54 +0100

    added js, react, redux icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,6 +3,9 @@ import { Container, Row, Col } from "reactstrap";
 import Title from './Title';
 import SubHeader2 from "./SubHeader2";
 import Animation from "../components/Animation";
+import Javascript from "../images/icons/js.svg";
+import ReactIcon from "../images/icons/react.svg";
+import ReduxIcon from "../images/icons/redux.svg";
 
 const Intro = props => {
 
@@ -18,15 +21,29 @@ const Intro = props => {
                <div style={styles.text}>
                  <SubHeader2
                    text={
-                     "I'm a UX problem-solver based in England. Currently designing at Brakes."
+                     "I'm a front-end developer/designer based in England. Currently designing at Brakes and coding freelance."
                    }
                  />
                </div>
              </Col>
              <Col xs="5" sm="6">
                <div className={"float-right"} style={styles.animContainer}>
-                <Animation />
+                 <Animation />
                </div>
+             </Col>
+           </Row>
+           <Row>
+             <Col style={styles.iconContainer} xs="4" lg="2">
+               <img
+                 style={styles.icons}
+                 src={Javascript}
+               />
+             </Col>
+             <Col style={styles.iconContainer} xs="4" lg="2">
+               <img style={styles.icons} src={ReactIcon} />
+             </Col>
+             <Col style={styles.iconContainer} xs="4" lg="2">
+               <img style={styles.icons} src={ReduxIcon} />
              </Col>
            </Row>
          </Container>
@@ -39,6 +56,13 @@ const styles = {
   container: {
     marginTop: "7.5%",
     marginBottom: "8%",
+  },
+  iconContainer:{
+
+  },
+  icons: {
+    width: "50%",
+    marginTop: "15%"
   },
   text: {
     paddingTop: "10%",
